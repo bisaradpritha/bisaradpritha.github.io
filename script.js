812e85bb226c1149bddcdecf013e0b11d75e54fe
Pritha Bisarad <51969517+bisaradpritha@users.noreply.github.com>
2026-07-21 12:03:22 -0700

    Add VANTA.NET effect to hero element

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+VANTA.NET({
+
+    el:"#hero",
+
+    mouseControls:true,
+
+    touchControls:true,
+
+    gyroControls:false,
+
+    scale:1,
+
+    scaleMobile:1,
+
+    color:0x5de4ff,
+
+    backgroundColor:0x08090d,
+
+    points:12,
+
+    maxDistance:20,
+
+    spacing:18
+
+});
