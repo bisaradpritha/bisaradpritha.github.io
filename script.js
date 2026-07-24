940e77c953b657661e73abe47def8fc168c948d7
Pritha Bisarad <51969517+bisaradpritha@users.noreply.github.com>
2026-07-24 12:30:08 -0700

    Add FinisherHeader configuration to script.js

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
+new FinisherHeader({
 
+    count:100,
+
+    size:{
+        min:2,
+        max:13,
+        pulse:0
+    },
+
+    speed:{
+        x:{
+            min:0,
+            max:0.4
+        },
+        y:{
+            min:0,
+            max:0.6
+        }
+    },
+
+    colors:{
+        background:"#0d0320",
+        particles:[
+            "#bb5579",
+            "#ae1249"
+        ]
+    },
+
+    blending:"overlay",
+
+    opacity:{
+        center:1,
+        edge:0
+    },
+
+    skew:-2,
+
+    shapes:["c"]
+
+});
 
