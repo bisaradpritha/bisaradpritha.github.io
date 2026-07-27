9c29bcd1b78c182dce3ace4ff51b06da3abc7e3d
Pritha Bisarad <51969517+bisaradpritha@users.noreply.github.com>
2026-07-27 13:36:57 -0700

    Refactor cursor particle creation and animation

--- cursor.js
+++ cursor.js
@@ -1,3 +1,5 @@
+(() => {
+
 const trail = document.getElementById("cursor-trail");
 
 const colors = [
@@ -11,57 +13,55 @@ const colors = [
 let mouseX = 0;
 let mouseY = 0;
 
-document.addEventListener("mousemove",(e)=>{
+document.addEventListener("mousemove", (e) => {
 
-    mouseX=e.clientX;
-    mouseY=e.clientY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 
 });
 
-function createParticle(){
+function createParticle() {
 
-    const p=document.createElement("div");
+    const p = document.createElement("div");
 
-    p.className="cursor-particle";
+    p.className = "cursor-particle";
 
-    const size=2+Math.random()*6;
+    const size = 2 + Math.random() * 6;
 
-    p.style.width=size+"px";
-    p.style.height=size+"px";
+    p.style.width = size + "px";
+    p.style.height = size + "px";
 
-    p.style.left=mouseX+"px";
-    p.style.top=mouseY+"px";
+    p.style.left = mouseX + "px";
+    p.style.top = mouseY + "px";
 
-    p.style.opacity=.15+Math.random()*.3;
+    p.style.opacity = .15 + Math.random() * .3;
 
-    p.style.background=
-        colors[Math.floor(Math.random()*colors.length)];
+    p.style.background =
+        colors[Math.floor(Math.random() * colors.length)];
 
-    p.style.boxShadow=
-        `0 0 ${size*3}px ${p.style.background}`;
+    p.style.boxShadow =
+        `0 0 ${size * 3}px ${p.style.background}`;
 
     p.style.setProperty("--dx",
-        (Math.random()*18-9)+"px");
+        (Math.random() * 18 - 9) + "px");
 
     p.style.setProperty("--dy",
-        (Math.random()*18-9)+"px");
+        (Math.random() * 18 - 9) + "px");
 
     trail.appendChild(p);
 
-    setTimeout(()=>{
-
-        p.remove();
-
-    },800);
+    setTimeout(() => p.remove(), 800);
 
 }
 
-function animate(){
+function animateCursor() {
 
     createParticle();
 
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animateCursor);
 
 }
 
-animate();
+requestAnimationFrame(animateCursor);
+
+})();
